91060cc852d45373b740037147ea5d113e96d7e5
koceila ouikene <koceila.ouikene@gmail.com>
2023-10-22 12:15:29 +0200

    init app

---
+++ src/main/java/com/akessil/questionapp/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.akessil.questionapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="question-app")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public @ResponseBody String hello(@RequestParam(value = "name") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping(value = "/helloWorld")
+    public @ResponseBody QuestionDto helloWorld() {
+        QuestionDto q = new QuestionDto();
+        q.setText("Hello World");
+        return q;
+    }
+}
---
+++ src/main/java/com/akessil/questionapp/controller/QuestionDto.java
@@ -0,0 +1,13 @@
+package com.akessil.questionapp.controller;
+
+public class QuestionDto {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
